4d9fa49e1c69aa6e9b51956947c5a0a808cbec16
dennisdang17 <dennisdang17@gmail.com>
2021-07-17 22:43:56 -0700

    ?

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import json
 import os
 import random
 import time
+import urllib.request
 from collections import OrderedDict
 
 import discord
@@ -28,7 +29,8 @@ async def on_member_join(member):
     guild = client.get_guild(833127705054674965)
     await member.channel.send(f'Welcome to the server {member.name}!')
 
-@client.event #!joke command
+
+@client.event
 async def on_message(message):
 
     value = random.randint(1,10)
@@ -48,6 +50,7 @@ async def on_message(message):
 
     if message.content == '!joke':
         jokes = json.loads(requests.get(r"https://official-joke-api.appspot.com/random_joke").text)
+        print(jokes)
         await message.channel.send(jokes["setup"])
         time.sleep(3)
         await message.channel.send(jokes["punchline"])
@@ -57,22 +60,32 @@ async def on_message(message):
         await message.channel.send(data[0]["death"])
 
     if message.content == '!cokeTime':
-        await message.channel.send(file=discord.File("/lib/cokeTime.gif"))
+        await message.channel.send(file=discord.File("cokeTime.gif"))
 
     #TODO Send a picture of the league champion and then list all of the base stats in a message
     #using discord embed
     if message.content.startswith('!leaguestats'):
         message_string = message.content.split(" ")
         try:
-            base_stat = message_string[1]
+            champion = message_string[1]
             data_list = get_champ_data_list()
-            sorted_list = OrderedDict()
-            for champ in data_list:
-                sorted_list[champ] = data_list[champ]['stats'][base_stat]
-            sorted_list = sorted(sorted_list.items(), key = lambda x: int(x[1]), reverse=True)
-            await message.channel.send(sorted_list[0])
+            await message.channel.send(champion)
+            await message.channel.send(data_list[champion]['stats'])
         except IndexError:
             await message.channel.send("Uh oh! I need a stat to look up! Usage: '!leaguestats hp'")
+            """
+            
+            sorted_list = OrderedDict()
+            for champ in data_list:
+                await message.channel.send(base_stat)
+                await message.channel.send(" : ")
+                await message.channel.send(data_list[base_stat])
+                sorted_list[champ] = data_list[champ]['stats'][base_stat]
+            sorted_list = sorted(sorted_list.items(), key = lambda x: int(x[1]), reverse=True)
+            #await message.channel.send(sorted_list[champ])
+        except IndexError:
+            await message.channel.send("Uh oh! I need a stat to look up! Usage: '!leaguestats hp'")
+            """
 
 
 def get_champ_data_list():
@@ -81,6 +94,48 @@ def get_champ_data_list():
     current_champ_list = lol_watcher.data_dragon.champions(champions_version)
     champion_data_list = OrderedDict(current_champ_list['data'])
     return champion_data_list
+
+def get_champ_portraits():
+    #coding:utf8    
+    cur_path = os.path.abspath(os.curdir)
+    
+    champion = "https://game.gtimg.cn/images/lol/act/img/js/heroList/hero_list.js"
+    championobj =  json.loads(requests.get(champion).text)
+    heroIds = championobj['hero']
+    # print(championobj)
+    # # print(jsonobj)
+    for c in heroIds:
+        print(c["heroId"])
+        url="https://game.gtimg.cn/images/lol/act/img/js/hero/"+str(c["heroId"])+".js"
+        # Send request, get response result
+        response = requests.get(url)
+        text = response.text
+    
+            # Print the response content of this request
+        # print(text)
+    
+    # Convert response content into Json object
+        jsonobj = json.loads(text)
+        # print(jsonobj)
+            #Get hero name as folder name
+        heroname =  jsonobj["hero"]["name"] 
+        print(heroname) 
+        isExists=os.path.exists(cur_path+'/all skin pictures/'+heroname)
+        skinpath =cur_path+'/all skin pictures/'+heroname
+        if not isExists:
+                    os.makedirs(cur_path+'/all skin pictures/'+heroname)
+        # print(heroname)
+            #Get all skins
+        heroskins = jsonobj["skins"]
+        # print(heroskins)
+        for s in heroskins:
+            skinname = str(s["name"]).replace('','-').replace('/','') #skin name
+            skinimage = str(s["mainImg"]) #Skin image path
+            if(len(skinimage)!=0): 
+                file_suffix = os.path.splitext(skinimage)[1]
+                filename = '{}{}{}{}'.format(skinpath,os.sep,skinname,file_suffix)
+                urllib.request.urlretrieve(skinimage,filename)
+            # print(skinimage)
 
 def main():
     client.run(TOKEN)
